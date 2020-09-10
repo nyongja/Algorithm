a2f3de07b2716feeb04a4ac5cd8ef1dc5c4e59cd
Jayun Yong <33798183+nyongja@users.noreply.github.com>
2020-09-10 12:39:53 +0900

    3-3 숫자 카드 게임
    2019 국가 교육기관 코딩 테스트

---
+++ This is Coding Test/Ch3 Greedy/3-3.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__" :
+    n, m = map(int, input().split())
+    max_card = 0 
+    for i in range(n) :
+        card = list(map(int, input().split()))
+        min_card = min(card)
+        max_card = max(min_card, max_card)
+    print(max_card)
