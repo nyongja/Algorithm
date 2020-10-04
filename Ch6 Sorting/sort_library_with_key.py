5c1e3ec0f322e84a14ce53e94099985e534b1845
nyongja <nyongja@naver.com>
2020-10-04 16:39:46 +0900

    sort library with key

---
+++ Ch6 Sorting/sort_library_with_key.py
@@ -0,0 +1,10 @@
+# sorted는 리스트 자료형 반환
+# sort는 반환 없이 내부 원소 바로 정렬
+
+array = [('바나나', 2), ('사과', 5), ('당근', 3)]
+
+def setting(data):
+    return data[1]
+
+result = sorted(array, key = setting)
+print(result)
