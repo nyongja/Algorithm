7d5daebcab592a5dcd0ffef670ef0579b0cea0df
nyong <nyongja@naver.com>
2020-10-06 12:13:51 +0900

    7-1 부품찾기

---
+++ Ch7 Binary Search/7-1.py
@@ -0,0 +1,19 @@
+def binary_search(sell_list, item, start, end) :
+    while start <= end :
+        mid = (start + end) // 2
+        if item == sell_list[mid] :
+            return "yes"
+        elif item < mid :
+            end = mid - 1
+        else :
+            start = mid + 1
+    return "no"
+
+if __name__ == "__main__" :
+    n = int(input())
+    sell_list = list(map(int, input().split()))
+    m = int(input())
+    buy_list = list(map(int, input().split()))
+
+    for i in buy_list :
+        print(binary_search(sell_list, i, 0, n-1))
