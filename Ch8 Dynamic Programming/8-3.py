c7abcf93be68bfb5e24d456da9661052eb0ab018
nyongja <nyongja@naver.com>
2020-10-21 12:40:01 +0900

    8-3 바닥 공사

---
+++ Ch8 Dynamic Programming/8-3.py
@@ -0,0 +1,21 @@
+# 8-3 바닥공사
+
+def solution(n) :
+    d = [0] * 1001
+
+    d[1] = 1
+    d[2] = 3
+    if n == 1 or n == 2 : return d[n]
+
+    for i in range(3, n + 1) :
+        d[i] += d[i-1] * 1
+        d[i] += d[i-2] * 2
+        d[i] = d[i] % 796796
+
+    return d[n]
+
+
+
+if __name__ == "__main__" :
+    n = int(input())
+    print(solution(n))
