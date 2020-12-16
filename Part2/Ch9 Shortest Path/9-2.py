313881c90eb6c65db99ccb3b5c84cb5ba9f17466
nyongja <nyongja@naver.com>
2020-12-16 11:45:39 +0900

    9-2 전보

--- Part2/Ch9 Shortest Path/9-2.py
+++ Part2/Ch9 Shortest Path/9-2.py
@@ -8,7 +8,7 @@ def dijkstra(start) :
     distance[start] = 0
     while q :
         dist, now = heapq.heappop(q)
-        if distance[now] < dist :
+        if distance[now] < dist : 
             continue
         for i in graph[now] :
             cost = dist + i[1]
