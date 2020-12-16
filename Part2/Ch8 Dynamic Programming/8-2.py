4496d037b0a5133778151c46947103d1cc0687e2
nyongja <nyongja@naver.com>
2020-12-16 11:43:11 +0900

    8-2 개미전사

--- Part2/Ch8 Dynamic Programming/8-2.py
+++ Part2/Ch8 Dynamic Programming/8-2.py
@@ -1,7 +1,7 @@
 # 개미 전사
 def solution(food_storage):
     answer = 0
-    d = [0] * 100
+    d = [0] * 100 
     
     for index, value in enumerate(food_storage):
         if index == 0 or index == 1 :
