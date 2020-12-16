647fbebe563d9ffabf9aa3af1517a9fd01f1c0db
nyongja <nyongja@naver.com>
2020-12-16 11:32:32 +0900

    DFS example

--- Part2/Ch5 DFS_BFS/dfs_ex.py
+++ Part2/Ch5 DFS_BFS/dfs_ex.py
@@ -6,7 +6,7 @@ def dfs(graph, v, visited):
     # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
     for i in graph[v]:
         if not visited[i]:
-            dfs(graph, i, visited)
+            dfs(graph, i, visited) 
 
 
 # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
