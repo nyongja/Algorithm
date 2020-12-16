95ef13464ca20b348313558a0c4a6d72860f25a8
nyongja <nyongja@naver.com>
2020-12-16 11:44:28 +0900

    피보나치 수열(재귀 ver.)

--- Part2/Ch8 Dynamic Programming/fibonacci_func_memoization_ex.py
+++ Part2/Ch8 Dynamic Programming/fibonacci_func_memoization_ex.py
@@ -9,7 +9,7 @@ def fibo(x) :
         return 1
     # 이미 계산한 적 있는 문제라면 그대로 반환
     if d[x] != 0 :
-        return d[x]
+        return d[x] 
     # 아직 계산하지 않은 문제라면 점화식에 따라서 피보나치 결과 반환
     d[x] = fibo(x - 1) + fibo(x - 2)
     return d[x]
