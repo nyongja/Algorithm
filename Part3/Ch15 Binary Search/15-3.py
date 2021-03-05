7584664a1b0fb181bbe46be76d572e56b1a312e0
자윤 <nyongja@jayun-ui-MacBookAir.local>
2021-03-06 01:11:18 +0900

    15-3 공유기 설치

--- Part3/Ch15 Binary Search/15-3.py
+++ Part3/Ch15 Binary Search/15-3.py
@@ -2,11 +2,12 @@
 
 
 # 집 개수, 공유기 개수 input
-n, c = map(int, input().split())
+n, c = list(map(int, input().split(' ')))
 house = [int(input()) for _ in range(n)]
 house.sort()
 
-start = house[1] - house[0] # 집의 좌표 중에 가장 작은 값
+#start = house[1] - house[0] # 집의 좌표 중에 가장 작은 값
+start = 1
 end = house[-1] - house[0] # 집의 좌표 중에 가장 큰 값
 result = 0
 
